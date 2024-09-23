1971f152bc617c4e2a15511c414273a9d4043313
Sleepergamedev <gustav.sanden996@gmail.com>
2024-09-23 14:42:01 +0200

    fix2

--- Flip24/Assets/Scripts/TableShoot.cs
+++ Flip24/Assets/Scripts/TableShoot.cs
@@ -10,7 +10,6 @@ public class TableShoot : MonoBehaviour
     public float torque;
     public Vector3 throwDirection;
     [SerializeField] Rigidbody2D rb;
-    int test = 5;
 
     void FixedUpdate()
     {
