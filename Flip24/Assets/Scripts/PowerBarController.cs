362c10da53a2d1b18477d71bb8afdaa65a063cde
Sleepergamedev <gustav.sanden996@gmail.com>
2024-09-23 14:31:49 +0200

    rotation

--- Flip24/Assets/Scripts/PowerBarController.cs
+++ Flip24/Assets/Scripts/PowerBarController.cs
@@ -1,34 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PowerBarController : MonoBehaviour
 {
-    public Slider powerBar; 
+    public Slider powerBar;
     public float powerBarSpeed;
     public float setPower;
+    TableShoot shootScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shootScript = FindFirstObjectByType<TableShoot>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        powerBar.value += powerBarSpeed *2 *Time.deltaTime;
+        powerBar.value += powerBarSpeed * 2 * Time.deltaTime;
         if (powerBar.value >= powerBar.maxValue || powerBar.value <= powerBar.minValue)
         {
             powerBarSpeed = -powerBarSpeed;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            setPower = powerBar.value;
-            powerBarSpeed = 0; 
-            
+            setPower = powerBar.value / 3;
+            Debug.Log(setPower);
+            shootScript.throwStrength = setPower;
+            powerBarSpeed = 0;
+
         }
 
     }
