b20939305ceb1ef4a90d73650c22787a741ab270
Chochx <143450790+Chochx@users.noreply.github.com>
2024-09-23 13:33:46 +0200

    .

--- Flip24/Assets/Scripts/CameraScript.cs
+++ Flip24/Assets/Scripts/CameraScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    public Camera mainCamera;
+    Vector2 cameraOffset = new Vector2(0.4f, 0.2f);
+    Rigidbody2D rb; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera.transform.position = transform.position + (Vector3)cameraOffset;
+        rb = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mainCamera.transform.position = Vector2.Lerp(mainCamera.transform.position, transform.position, 0.8f) * Time.deltaTime;
+        rb.AddForce(new Vector2(2,2), ForceMode2D.Impulse);
+
     }
 }
