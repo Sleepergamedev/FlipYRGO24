df203a8259e19bfad36f62c16977a70f36bc1c38
Sleepergamedev <gustav.sanden996@gmail.com>
2024-09-24 12:58:43 +0200

    .

--- Flip24/Assets/Scripts/TableShoot.cs
+++ Flip24/Assets/Scripts/TableShoot.cs
@@ -12,21 +12,20 @@ public class TableShoot : MonoBehaviour
     public Vector3 throwDirection;
     [SerializeField] Rigidbody2D rb;
     public int spaceButtonPressed;
-    CameraScript cameraScript; 
-    
+    CameraScript cameraScript;
+
 
 
     private void Start()
     {
         cameraScript = FindFirstObjectByType<CameraScript>();
-        
+
     }
     void Update()
     {
         //if sats för att skjuta iväg spelaren beroende på spelarens input
         if (spaceButtonPressed == 2 && cameraScript.launchReady)
         {
-            
             rb.AddForce(throwDirection.normalized * throwStrength, ForceMode2D.Impulse);
             rb.AddTorque(-torque, ForceMode2D.Force);
             spaceButtonPressed = 0;
