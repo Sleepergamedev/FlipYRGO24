be0c35c8cf08b693dd42888a308298583329e765
Sleepergamedev <gustav.sanden996@gmail.com>
2024-09-24 13:42:10 +0200

    la in skjut animation

--- Flip24/Assets/Scripts/PlayerAnimationManager.cs
+++ Flip24/Assets/Scripts/PlayerAnimationManager.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class PlayerAnimationManager : MonoBehaviour
 {
     private Animator animator;
+    CameraScript cameraScript;
 
     void Start()
     {
+        cameraScript = FindFirstObjectByType<CameraScript>();
         animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x == -2.1)
+        if (transform.position.x == -2.1 && cameraScript.launchReady)
         {
-
+            animator.Play("MainCharacterThrow");
         }
     }
 }
