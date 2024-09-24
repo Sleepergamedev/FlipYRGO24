22b5eec5be1c072c542fa908ff6f68a123259f93
Chochx <143450790+Chochx@users.noreply.github.com>
2024-09-24 10:49:18 +0200

    UI

---
+++ Flip24/Assets/Scripts/UIScript.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class UIScript : MonoBehaviour
+{
+    [Header ("Buttons")]
+    public Button playButton;
+    public Button creditsButton;
+    public Button exitButton; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(LoadScene);
+        creditsButton.onClick.AddListener(CreditsWindow); 
+        exitButton.onClick.AddListener(ExitGame);
+    }
+
+    private void CreditsWindow()
+    {
+        
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single); 
+    }
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    
+}
