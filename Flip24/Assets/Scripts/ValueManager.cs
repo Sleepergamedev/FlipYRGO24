acc068ced4fe933f31ad54282a60cf2317dfc886
Sleepergamedev <gustav.sanden996@gmail.com>
2024-09-25 11:25:58 +0200

    la in spel slut samt en ui manager och lite grafik

--- Flip24/Assets/Scripts/ValueManager.cs
+++ Flip24/Assets/Scripts/ValueManager.cs
@@ -9,6 +9,7 @@ public class ValueManager : MonoBehaviour
     public bool isFlipped180;
     public bool isFlipped90;
     public bool isFlipped270;
+    public bool isGameOver = false;
     [SerializeField] Rigidbody2D rb;
 
 
@@ -20,18 +21,22 @@ public class ValueManager : MonoBehaviour
         if (transform.position.x > 0 && rb.velocity.x <= 0f)
         {
             metresFlown = Mathf.RoundToInt(transform.position.x);
+            isGameOver = true;
         }
         if (transform.localEulerAngles.z >= 179 && transform.localEulerAngles.z <= 181 && isFlipped180 == false && rb.velocity.x <= 0)
         {
             isFlipped180 = true;
+            isGameOver = true;
         }
         if (transform.localEulerAngles.z >= 89 && transform.localEulerAngles.z <= 91 && isFlipped90 == false && rb.velocity.x <= 0)
         {
             isFlipped90 = true;
+            isGameOver = true;
         }
         if (transform.localEulerAngles.z >= 269 && transform.localEulerAngles.z <= 271 && isFlipped270 == false && rb.velocity.x <= 0)
         {
             isFlipped270 = true;
+            isGameOver = true;
         }
     }
 }
