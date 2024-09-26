4873a573008ac91d60e01a759552992bccc6a0ce
Chochx <143450790+Chochx@users.noreply.github.com>
2024-09-26 14:15:01 +0200

    Changes in StartScene

--- Flip24/Assets/Scripts/UIScript.cs
+++ Flip24/Assets/Scripts/UIScript.cs
@@ -1,23 +1,46 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
+using System;
 
 public class UIScript : MonoBehaviour
 {
     [Header ("Buttons")]
     public Button playButton;
     
-    public Button exitButton; 
+    public Button exitButton;
+
+    public Button creditsButton;
+
+    public GameObject creditsPanel;
+
+    //private
+    bool toggleCredits; 
 
     // Start is called before the first frame update
     void Start()
     {
         playButton.onClick.AddListener(LoadScene);
-        
+
         exitButton.onClick.AddListener(ExitGame);
+
+        creditsButton.onClick.AddListener(ToggleCredits);
     }
 
+    private void ToggleCredits()
+    {
+        toggleCredits = !toggleCredits;
+        ToggleCredits(toggleCredits); 
+    }
 
+    private void ToggleCredits(bool toggleCredits)
+    {
+        if (toggleCredits)
+        {
+            creditsPanel.SetActive(true);
+        }
+        else { creditsPanel.SetActive(false); }
+    }
 
     void LoadScene()
     {
